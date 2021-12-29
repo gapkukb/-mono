14a7ca7b6e68a81c95e05f7c0bddbcb7ff9d8fb5
carlos <carlos>
2021-12-29 23:06:09 +0800

    update

---
+++ packages/a/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Test from "@mono/b/src/test";
+function App() {
+	return (
+		<div className="App">
+			<Test></Test>
+		</div>
+	);
+}
+
+export default App;
